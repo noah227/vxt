ac52797b5a81e5869441b262d2fa47f6a25d8113
noah227 <noah227@foxmail.com>
2024-05-10 17:22:08 +0800

    feat(directives): i18n directive support substitutions now

--- packages/directives/README.md
+++ packages/directives/README.md
@@ -17,6 +17,10 @@ app.use(vxtDirectives)
 ```vue
 <!--message got with chrome.i18n.getMessage('optionText') will be set as tag's innerHTML-->
 <a v-i18n="'optionText'"></a>
+
+<!--i18n with substitutions (chrome.i18n.getMessage('optionText', 'hello')-->
+<a v-i18n="['optionText', 'hello']"></a>
+
 <!--message got with chrome.i18n.getMessage('linkTitle') will be set as tag's title-->
 <a v-i18n:title="'linkTitle'"></a>
 ```
--- packages/directives/src/index.ts
+++ packages/directives/src/index.ts
@@ -1,14 +1,24 @@
 import {App, DirectiveBinding} from "vue";
 
+type TIi18nValue = string | [
+    key: string,
+    substitutions?: string | string[]
+]
 /**
  * 快捷的i18n指令
  * @description set with message got with chrome.i18n.getMessage,
- * default set to innerHTML, does not support substitutions
+ * default set to innerHTML, support substitutions
  */
 export const directiveI18n = {
-    created(el: HTMLElement, {value: key, arg = ""}: DirectiveBinding) {
-        if (!key) return
-        const msg = chrome.i18n.getMessage(key)
+    created(el: HTMLElement, {value, arg = ""}: DirectiveBinding<TIi18nValue>) {
+        let key = "", substitutions = undefined
+        if (value instanceof Array) {
+            if (!value.length) return
+            key = value[0]
+            if (value.length > 1) substitutions = value[1]
+        } else if (typeof value === "string") key = value
+        else return
+        const msg = chrome.i18n.getMessage(key, substitutions)
         if (arg && arg !== "html") {
             el.setAttribute(arg, msg)
         } else el.innerHTML = msg
